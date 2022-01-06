9ccbd110ca80a6d3e77e996920866ca80bf3cc75
danylo <logolevel@gmail.com>
2022-01-06 16:15:44 +0200

    Format JS code

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ $(function () {
 
 
 $(function () {
-	let button 			 = $('#button'),
-		$listOfElements  = $('.rowheight'),
+	let button = $('#button'),
+		$listOfElements = $('.rowheight'),
 		numberOfElements = $listOfElements.length;
 
-	let checker = new Promise((resolve, reject) => { 
+	let checker = new Promise((resolve, reject) => {
 
 		for (let i = 0; i < numberOfElements; i++) {
 			let valueCity = $(".rowheight td:nth-child(7)")[i].textContent;
